467364c82be29e7f30d0368ff9b6b9e1418343a9
nathangong <nathangong1234@gmail.com>
2022-08-28 17:14:52 -0400

    Fix backend url

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "https://boxdrop-backend-4fcpnpqgza-uc.a.run.app/";
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://boxdrop-backend-4fcpnpqgza-uc.a.run.app";
